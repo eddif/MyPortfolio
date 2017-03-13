52960279bd60b746700779c9f0b8fbe1f6c9563d
eddifey <eddifey@gmail.com>
2017-03-13 09:58:27 -0700

    Update Bootstrap, JQuery, Newtonsoft

--- myprofile/App_Start/BundleConfig.cs
+++ myprofile/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace myprofile
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-2.1.4.js"));
+                        "~/Scripts/jquery-3.1.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
