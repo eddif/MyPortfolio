8ea419c344e4cbda5e929dad94bf15af181e160a
eddifey <eddifey@gmail.com>
2017-03-13 08:28:54 -0700

    2017 revision

--- myprofile/App_Start/BundleConfig.cs
+++ myprofile/App_Start/BundleConfig.cs
@@ -19,6 +19,9 @@ namespace myprofile
                         "~/Scripts/custom.js",
                         "~/Scripts/jquery.magnific-popup.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                        "~/Scripts/site.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ myprofile/Scripts/site.js
@@ -0,0 +1,25 @@
+﻿$(window).scroll(function () {
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+    }
+});
+
+$(function() {
+    $('.nav-item a').click(function () {
+
+        localStorage.setItem('thisLink', $(this).parent().attr("id"));
+
+        $('.nav-item a').siblings().removeClass('active_nav');
+     
+    });
+});
+
+
+
+   
+
+
+
